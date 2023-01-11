4a78b4986c1fe8a5491c4daf58d1e45c3924384a
stephenspol <stephenssspol@gmail.com>
2023-01-11 18:41:26 -0500

    Refactored NBT Tag

--- nbt/TagType.java
+++ nbt/TagType.java
@@ -24,7 +24,7 @@ public enum TagType {
     /** A array of modified UTF-8 string. Length of string is a unsigned short. */
     TAG_STRING("TAG_STRING", StringTag.class),
     /** A list of nameless tags, all the same type. The list starts with {@code tagId} (1 byte), then the length of the list as a signed integer*/
-    TAG_LIST("TAG_LIST", (Class<?>) ListTag.class),
+    TAG_LIST("TAG_LIST", ListTag.class),
     /** A list of named tags. */
     TAG_COMPOUND("TAG_COMPOUND", CompoundTag.class),
     /** A array of signed integers. Length of array is a signed integer. */
@@ -34,14 +34,15 @@ public enum TagType {
 
     private final Class<?> tagClass;
     private final String typeName;
-    private final int id;
+
+    private static final String NOT_FOUND = " is not found!";
 
     private static final Map<Class<?>, TagType> BY_CLASS = new HashMap<>();
     private static final Map<String, TagType> BY_NAME = new HashMap<>();
     private static final TagType[] BY_ID = values();
 
     static {
-        for (TagType type : TagType.values()) {
+        for (TagType type : BY_ID) {
             BY_CLASS.put(type.getTagClass(), type);
             BY_NAME.put(type.getTypeName(), type);
         }
@@ -50,7 +51,6 @@ public enum TagType {
     private TagType (String typeName, Class<?> tagClass) {
         this.tagClass = tagClass;
         this.typeName = typeName;
-        this.id = values().length;
     }
 
     public Class<?> getTagClass() {
@@ -61,14 +61,10 @@ public enum TagType {
         return typeName;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public static TagType getByTagClass(Class<?> clazz) {
-        TagType ret = BY_CLASS.get(clazz);
+    public static TagType getByTagClass(Class<?> cls) {
+        TagType ret = BY_CLASS.get(cls);
         if (ret == null) {
-            throw new IllegalArgumentException("Tag type " + clazz + " is unknown!");
+            throw new IllegalArgumentException("Tag type " + cls + NOT_FOUND);
         }
         return ret;
     }
@@ -76,7 +72,7 @@ public enum TagType {
     public static TagType getByTypeName(String typeName) {
         TagType type = BY_NAME.get(typeName);
         if (type == null) {
-            throw new IllegalArgumentException("Tag type " + typeName + " is unknown!");
+            throw new IllegalArgumentException("Tag type " + typeName + NOT_FOUND);
         }
         return type;
     }
@@ -85,11 +81,21 @@ public enum TagType {
         if (id >= 0 && id < BY_ID.length) {
             TagType tag = BY_ID[id];
             if (tag == null) {
-                throw new IllegalArgumentException("Tag type id " + id + " is unknown!");
+                throw new IllegalArgumentException("Tag type id " + id + NOT_FOUND);
             }
             return tag;
         } else {
             throw new IndexOutOfBoundsException("Tag type id " + id + " is out of bounds!");
         }
+    }
+
+    public static int getIdByTagType(TagType tag) {
+        for (int i = 0; i < BY_ID.length; i++) {
+            if (tag.name().equals(BY_ID[i].name())) {
+                return i;
+            }
+        }
+
+        throw new IllegalArgumentException("Tag Type " + tag.name() + " does not exist!");
     }
 }
